0ee67829f47894b1c91d1219a0a3d362f36d7843
thanh <thanhadp2402@gmail.com>
2025-12-07 19:56:54 +0700

    Task #95712 trang danh sách citizen(phân trang, tìm kiếm)

--- src/main/java/vn/sun/public_service_manager/repository/CitizenRepository.java
+++ src/main/java/vn/sun/public_service_manager/repository/CitizenRepository.java
@@ -1,11 +1,25 @@
 package vn.sun.public_service_manager.repository;
 
 import vn.sun.public_service_manager.entity.Citizen;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 
 public interface CitizenRepository extends JpaRepository<Citizen, Long> {
     boolean existsByNationalId(String nationalId);
+
     boolean existsByEmail(String email);
+
     Optional<Citizen> findByNationalId(String nationalId); // Dùng cho đăng nhập
+
+    @Query("SELECT c FROM Citizen c WHERE " +
+            "c.fullName LIKE %?1% OR " +
+            "c.nationalId LIKE %?1% OR " +
+            "c.email LIKE %?1% OR " +
+            "c.phone LIKE %?1%")
+    Page<Citizen> findByKeyword(String keyword, Pageable pageable);
 }
